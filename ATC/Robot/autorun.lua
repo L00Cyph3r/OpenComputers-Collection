1263ec54d71388b5d54abd5c554478eed2ba6a80
Lucien Plattel <lucienplattel@twinmediabv.nl>
2017-01-13 16:33:54 +0100

    Sleep some more!

--- ATC/Robot/autorun.lua
+++ ATC/Robot/autorun.lua
@@ -1,7 +1,11 @@
 local c = require("component")
 local shell = require("shell")
 shell.execute("clear")
-os.sleep(3)
+os.sleep(1)
+os.sleep(1)
+os.sleep(1)
+os.sleep(1)
+os.sleep(1)
 shell.execute("/listener.lua")
 
 
